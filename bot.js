68137bc3fdc45ed5046bf93060866ee46dbc9a8b
Troll Kim <ikim0106@hotmail.com>
2017-06-17 12:52:59 +0900

    censored google custom search key

--- bot.js
+++ bot.js
@@ -1,9 +1,15 @@
 const { Wechaty } = require('wechaty') // import { Wechaty } from 'wechaty'
 var request = require("request");
+var auth = require('./auth.json')
 
 function getRoom(message)
 {
     return message.room()
+}
+
+function roomName(message)
+{
+
 }
 
 function getContent(message)
@@ -38,25 +44,13 @@ Wechaty.instance()
     } 
 })
 
-.on('message', message=>{
-    var sentmessage2 = message.toString();
-    sentmessage2 = sentmessage2.toLowerCase();
-    sentmessage2 = sendmessage2.trim();
-    if(sentmessage.includes('get')&&!(message.self()))
-    {
-        sentmessage2 = sentmessage2.replace('get ', '')
-        if(sentmessage2==='services')
-        getRoom(message).say(`www.coderbunker.com/services`)
-        else if(sentmessage2==='list')
-        getRoom(message).say('services: link to coderbunker services page' + '\n' + '')
-    }
-})
+
 
 .on('room-join', user =>{
     //person joins the chat and the bot sends an appropriate message
 })
 
-.on('room-leave', user=>{
+.on('room-leave', user=>    {
     //person leaves the chat and the bot sends an appropriate message
 })
 
@@ -68,7 +62,7 @@ Wechaty.instance()
     if (imagerequest.includes('imgsearch')&&!(message.self()))
     {
         imagerequest = imagerequest.replace('imgsearch ','')
-        request("https://www.googleapis.com/customsearch/v1?key=+AIzaSyBiw58K8MJOzZYIbDNTcJN0aSw8K1MOtSI&cx=005095525797302733243:qcilnmzij7q&num=1&searchType=image&q=" + imagerequest,
+        request(auth.requesturl + imagerequest,
             function (err, res, body)
             {
                 var data, error;
